28ebe738444bd590314e2df6adec9028a7c363ba
xx <369889186@qq.com>
2021-04-09 10:47:54 +0800

    init

---
+++ src/an/sixtofly/Main.java
@@ -0,0 +1,13 @@
+package an.sixtofly;
+
+import java.util.HashMap;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // write your code here
+        HashMap<String, String> map = new HashMap<>();
+        map.put("xx", "xx");
+        System.out.println(map);
+    }
+}
