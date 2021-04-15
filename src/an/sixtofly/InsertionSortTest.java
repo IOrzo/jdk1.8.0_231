b295a647292cab46bd915ff330a2e84cdf378373
xx <369889186@qq.com>
2021-04-15 16:09:00 +0800

    插入排序

---
+++ src/an/sixtofly/InsertionSortTest.java
@@ -0,0 +1,28 @@
+package an.sixtofly;
+
+/**
+ * 插入排序, 与前面已排序数做比较
+ * 所有元素后移, 若当前数大于前面数, 则插入
+ *
+ * @author xie yuan bing
+ * @date 2021-04-14 17:21
+ */
+public class InsertionSortTest {
+
+    public static void main(String[] args) {
+        int[] data = AlgorithmsTest.random();
+        for (int i = 1; i < data.length; i++) {
+            int temp = data[i];
+            for (int m = i; m > 0; m--) {
+                if (temp < data[m - 1]) {
+                    data[m] = data[m - 1];
+                    data[m - 1] = temp;
+                } else {
+                    data[m] = temp;
+                    break;
+                }
+            }
+        }
+        AlgorithmsTest.print(data);
+    }
+}
