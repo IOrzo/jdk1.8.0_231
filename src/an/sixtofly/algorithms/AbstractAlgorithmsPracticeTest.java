0b9c7acb2b9fc50e7e9920e5daf10c7872e4e51d
xx <369889186@qq.com>
2021-06-22 14:06:41 +0800

    练习类

---
+++ src/an/sixtofly/algorithms/AbstractAlgorithmsPracticeTest.java
@@ -0,0 +1,19 @@
+package an.sixtofly.algorithms;
+
+/**
+ * @author xie yuan bing
+ * @date 2021-06-22 14:02
+ */
+public abstract class AbstractAlgorithmsPracticeTest {
+
+
+    public abstract void test(int data[]);
+
+
+    public void run() {
+        int[] data = AlgorithmsTest.random();
+        test(data);
+        AlgorithmsTest.print(data);
+    }
+
+}
---
+++ src/an/sixtofly/algorithms/AlgorithmsPracticeTest.java
@@ -0,0 +1,20 @@
+package an.sixtofly.algorithms;
+
+/**
+ * 基础算法练习
+ * @author xie yuan bing
+ * @date 2021-06-18 11:03
+ */
+public class AlgorithmsPracticeTest extends AbstractAlgorithmsPracticeTest {
+
+
+    public static void main(String[] args) {
+        AlgorithmsPracticeTest test = new AlgorithmsPracticeTest();
+        test.run();
+    }
+
+    @Override
+    public void test(int[] data) {
+
+    }
+}
