6575e7525e80017a8a9849cd122d264f93c54d78
xx <369889186@qq.com>
2021-06-24 17:40:32 +0800

    java spi

---
+++ src/an/sixtofly/spi/MobilePhone.java
@@ -0,0 +1,11 @@
+package an.sixtofly.spi;
+
+/**
+ * @author xie yuan bing
+ * @date 2021-06-24 17:09
+ * @description
+ */
+public interface MobilePhone {
+
+    void call(String msg);
+}
---
+++ src/an/sixtofly/spi/XiaoMi.java
@@ -0,0 +1,12 @@
+package an.sixtofly.spi;
+
+/**
+ * @author xie yuan bing
+ * @date 2021-06-24 17:11
+ */
+public class XiaoMi implements MobilePhone {
+    @Override
+    public void call(String msg) {
+        System.out.println("使用 XiaoMi 打电话:" + msg);
+    }
+}
