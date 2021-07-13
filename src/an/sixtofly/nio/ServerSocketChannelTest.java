21a0813a3b84735abee22b4d7b1d85c1119763da
xx <369889186@qq.com>
2021-07-13 17:09:42 +0800

    java nio 增加系统调用源代码

--- src/an/sixtofly/nio/ServerSocketChannelTest.java
+++ src/an/sixtofly/nio/ServerSocketChannelTest.java
@@ -7,8 +7,8 @@ import java.nio.channels.SelectionKey;
 import java.nio.channels.Selector;
 import java.nio.channels.ServerSocketChannel;
 import java.nio.channels.SocketChannel;
+import java.util.Arrays;
 import java.util.Iterator;
-import java.util.Set;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
@@ -19,8 +19,6 @@ import java.util.concurrent.Executors;
 public class ServerSocketChannelTest {
 
 
-
-
     private static ExecutorService dispatcher = Executors.newSingleThreadExecutor();
 
 
@@ -28,10 +26,8 @@ public class ServerSocketChannelTest {
 
         Selector selector = Selector.open();
 
-
-
         ServerSocketChannel serverSocketChannel = ServerSocketChannel.open();
-        serverSocketChannel.socket().bind(new InetSocketAddress(9527));
+        serverSocketChannel.socket().bind(new InetSocketAddress(9110));
         serverSocketChannel.configureBlocking(false);
         serverSocketChannel.register(selector, SelectionKey.OP_ACCEPT);
 
@@ -49,12 +45,93 @@ public class ServerSocketChannelTest {
             }
         }
 
+    }
 
 
+    /**
+     * 事件分发器
+     *
+     * @author xie yuan bing
+     * @date 2021-06-03 11:20
+     */
+    public static class DispatcherHandlerTest implements Runnable {
+
+        /**
+         * 事件处理器
+         */
+        private static ExecutorService executorService = Executors.newSingleThreadExecutor();
 
 
+        private Selector selector;
+
+        public DispatcherHandlerTest(Selector selector) {
+            this.selector = selector;
+        }
+
+        @Override
+        public void run() {
+            while (true) {
+                int readyChannels = 0;
+                try {
+                    readyChannels = selector.selectNow();
+                    if (readyChannels == 0) continue;
+                    Iterator<SelectionKey> iterator = selector.selectedKeys().iterator();
+                    while (iterator.hasNext()) {
+                        SelectionKey key = iterator.next();
+                        DispatcherHandlerTest.ServerSocketHandlerTest handler = new DispatcherHandlerTest.ServerSocketHandlerTest(key);
+                        executorService.execute(handler);
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
 
 
+        class ServerSocketHandlerTest implements Runnable {
+
+            private SelectionKey key;
+
+
+            public ServerSocketHandlerTest(SelectionKey selectionKeys) {
+                this.key = selectionKeys;
+            }
+
+            @Override
+            public void run() {
+
+                try {
+                    if (key.isAcceptable()) {
+                        System.out.println("key.isAcceptable()");
+                    } else if (key.isConnectable()) {
+                        System.out.println("key.isConnectable()");
+                    } else if (key.isReadable()) {
+                        System.out.println("key.isReadable()");
+                        SocketChannel socketChannel = (SocketChannel) key.channel();
+                        ByteBuffer buffer = ByteBuffer.allocate(50);
+                        socketChannel.read(buffer);
+                        buffer.flip();
+                        System.out.println("result:" + new String(buffer.array()));
+                        socketChannel.register(key.selector(), SelectionKey.OP_WRITE, buffer);
+                    } else if (key.isWritable()) {
+                        System.out.println("key.isWritable()");
+                        SocketChannel socketChannel = (SocketChannel) key.channel();
+                        ByteBuffer buffer = (ByteBuffer) key.attachment();
+                        buffer.flip();
+                        String result = new String(Arrays.copyOf(buffer.array(), buffer.limit() + 1));
+                        buffer.clear();
+                        String temp  = "server receiver:" + result;
+                        buffer.put(temp.getBytes("utf-8"));
+                        buffer.flip();
+                        socketChannel.write(buffer);
+                        socketChannel.register(key.selector(), SelectionKey.OP_READ);
+//                        key.cancel();
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
     }
 
 
